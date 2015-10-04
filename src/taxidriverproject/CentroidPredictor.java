5979f1441bc92acd9590b9dc5120378514996cca
Abhishek <abhishekgupta1408@yahoo.com>
2015-10-04 12:38:13 +0530

    Normalised the Data

--- src/taxidriverproject/CentroidPredictor.java
+++ src/taxidriverproject/CentroidPredictor.java
@@ -11,15 +11,16 @@ import taxidriverproject.TaxiDriverProject.DataNode;
 public class CentroidPredictor {
     DataNode SourceInfo;
     private ArrayList<DataPoint> centroids;
-    private ArrayList<Integer> crowdAtEachCentroid;
-    private ArrayList<Double> densityAtEachCentroid;
+    private ArrayList<Double> crowdAtEachCentroid;
+    private ArrayList<Double> densityAtEachCentroid, distanceFromSource, timeFromSource;
+    
     private DataPoint preferredCentroid;
     CentroidPredictor(TaxiDriverProject T)
     {
         SourceInfo = T.getSourceInfo();
         centroids = parseLatLon(T.getCentroids());
-        crowdAtEachCentroid = T.getCrowdAtEachCentroid();
-        densityAtEachCentroid = T.getDensityAtEachCentroid();   
+        crowdAtEachCentroid = normaliseCrowd(T.getCrowdAtEachCentroid());
+        densityAtEachCentroid = normaliseDouble(T.getDensityAtEachCentroid());   
     }
     CentroidPredictor()
     {
@@ -29,30 +30,42 @@ public class CentroidPredictor {
     {   
         int pos = 0;
         double maximum = 0.0;
+        distanceFromSource = new ArrayList<>();
+        timeFromSource = new ArrayList<>();
         for(int i = 0;i<centroids.size();i++)
         {
-            MapNode node = new MapNode(SourceInfo.lat,SourceInfo.lon,centroids.get(i).lat,centroids.get(i).lon);
-         
+            MapNode node = new MapNode(SourceInfo.lat,SourceInfo.lon,centroids.get(i).lat,centroids.get(i).lon); 
             MapEngine googleMaps = new MapEngine(node);
-          
-            double prob_success = hypothesis(densityAtEachCentroid.get(i),crowdAtEachCentroid.get(i),googleMaps.getDistance(),googleMaps.getTime());
+            distanceFromSource.add(googleMaps.getDistance());
+            timeFromSource.add(googleMaps.getTime());
+        }
+        distanceFromSource = normaliseDouble(distanceFromSource);
+        timeFromSource = normaliseDouble(timeFromSource);
+        for(int i=0;i<centroids.size();i++)
+        { 
+            double prob_success = hypothesis(densityAtEachCentroid.get(i),crowdAtEachCentroid.get(i),distanceFromSource.get(i),timeFromSource.get(i));
             if(prob_success>maximum)
             {
                 maximum = prob_success;
                 pos = i;
             }
         }
-         preferredCentroid = centroids.get(pos);
+        preferredCentroid = centroids.get(pos);
         return centroids.get(pos);
     }
 
-    private double hypothesis(double croudDensity,int crowd,double distance,double time)
+    private double hypothesis(double croudDensity,double crowd,double distance,double time)
     {
-        double sucess =  10.0 * croudDensity + 5.0*crowd - 10.0*distance - 10.0*time; //preformance reward
-        double prob = 1 - Math.exp(-sucess);                //logistic probabilty sigmoid function
-        prob = 1.0/prob;
-        return prob;
+        double success =  (croudDensity * crowd)/(distance * time);
+        System.out.println(croudDensity + " " + crowd + " " +distance + " "+ time);
+        return success;
     }
+    
+    
+    /**
+     * Utility Function
+     * 
+     */
     private ArrayList<DataPoint> parseLatLon(ArrayList<DataPoint> L)
     {
         double divisor = 1000000.0;
@@ -63,6 +76,38 @@ public class CentroidPredictor {
             temp = L.get(i);
             temp.lat = (temp.lat/divisor) + 37;
             temp.lon = -1 * ((temp.lon/divisor) + 122);
+            res.add(temp);
+        }
+        return res;
+    }
+    private ArrayList<Double> normaliseCrowd(ArrayList<Integer> C)
+    {
+        ArrayList<Double> res = new ArrayList<>();
+        double min = 1000, max = 0, temp;
+        for(int i=0;i<C.size();i++)
+        {
+            min = Math.min(min, C.get(i));
+            max = Math.max(max, C.get(i)); 
+        }
+        for(int i=0;i<C.size();i++)
+        {
+            temp = (C.get(i)- min)/(max-min);
+            res.add(temp);
+        }
+        return res;
+    }
+    private ArrayList<Double> normaliseDouble(ArrayList<Double> C)
+    {
+        ArrayList<Double> res = new ArrayList<>();
+        double min = Math.pow(2, 100), max = 0, temp;
+        for(int i=0;i<C.size();i++)
+        {
+            min = Math.min(min, C.get(i));
+            max = Math.max(max, C.get(i)); 
+        }
+        for(int i=0;i<C.size();i++)
+        {
+            temp = (C.get(i)- min)/(max-min);
             res.add(temp);
         }
         return res;
